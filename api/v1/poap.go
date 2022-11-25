c82200f1a2e9b6a9f4e025a2eb97e7ba446e673e
XuZhida <XuZhida@1459601448@qq.com>
2022-11-25 19:03:32 +0800

    增加主页接口

--- api/v1/poap.go
+++ api/v1/poap.go
@@ -13,7 +13,7 @@ type MyPoapRes struct {
 }
 
 type MainPagePoapReq struct {
-	g.Meta `path:"/poap/mainpage_list" method:"get" tags:"PoapService" summary:"Get the poap of main page"`
+	g.Meta `path:"/poap/mainpage_list" method:"post" tags:"PoapService" summary:"Get the poap of main page"`
 	From   int64
 	Count  int64
 }
